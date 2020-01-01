e6b6f8ba565aa332c65d214e7e6cf9b6cb63c4ee
brutekoon <brutekoon@gmail.com>
2020-01-01 23:48:48 +0900

    add encode + decode operation

--- 03_Devide_DB/test_List_DB.py
+++ 03_Devide_DB/test_List_DB.py
@@ -2,17 +2,52 @@
 import sys
 import os
 import hashlib
+import zlib
+import StringIO
 
-#바이러스 DB ==> 따로 다른 파일로 나눔.
+#바이러스 DB ==> 암호화된 virus.kmd에 존재
 VirusDB = []
 
 #프로그램 동작 중 실제로 사용하는 DB용 리스트
 vdb= []
 vsize = []
 
+
+#.kmd 파일을 복호화
+def DecodeKMD(fname):
+        try :
+                fp = open(fname, 'rb')
+                buf = fp.read()
+                fp.close()
+
+                buf2 = buf[:-32] #암호화 내용 분리
+                fmd5 = buf[-32:] # MD5 분리
+
+                f = buf2
+                for i in range(3):
+                        md5 = hashlib.md5()
+                        md5.update(f)
+                        f = md5.hexdigest()
+
+                if f != fmd5:
+                        raise SystemError
+
+                buf3 = ''
+                for c in buf2[4:] : #0XFF로 xor (복호화)
+                        buf3 += chr(ord(c) ^ 0XFF)
+
+                buf4 = zlib.decompress(buf3) # 압축해
+                return buf4
+        except:
+                pass
+
+        return None
+
+
 #virus.db 파일에서 악성코드 패턴을 읽음
 def LoadVirusDB() :
-	fp = open('virus.db', 'rb')
+	buf = DecodeKMD('virus.kmd')
+	fp = StringIO.StringIO(buf)
 	
 	while True:
 		line = fp.readline()
