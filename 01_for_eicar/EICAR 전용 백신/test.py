eeae442c45256c629f6a72a70190108e5d2a2446
brutekoon <brutekoon@gmail.com>
2020-01-01 14:09:55 +0900

    01_eicar

--- 01_for_eicar/EICAR 전용 백신/test.py
+++ 01_for_eicar/EICAR 전용 백신/test.py
@@ -1,4 +1,14 @@
+#-*- coding:utf-8 -*-
+import os
+
 fp = open('./eicar.txt', 'rb')
 fbuf = fp.read()
-print fbuf
 fp.close()
+#Signature 비교
+if fbuf[0:3] == 'X5O':
+    print "virus"
+    os.remove('./eicar.txt')
+else :
+    print "No virus"
+
+
