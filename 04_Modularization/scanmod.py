92e55cfc51fadb8f25f5aac5c0ee36e5626d9da7
brutekoon <brutekoon@gmail.com>
2020-01-02 23:01:57 +0900

    specifi location function

--- 04_Modularization/scanmod.py
+++ 04_Modularization/scanmod.py
@@ -30,3 +30,18 @@ def ScanMD5(vdb, vsize, fname):
                 ret, vname = SearchVDB(vdb,fmd5)
 
         return ret, vname
+
+#특정 위치 검색법을 이용하여 악성코드 검사
+#특정 위치 검색법은 특정 위치에 바이트 값을 통하여 악성 코드 검사
+def ScanStr(fp, offset, mal_str):
+        size = len(mal_str) #악성코드 진단 문자열 길이
+
+        #특정 위치에 악성코드 진단 문자열이 존재하는지 체크
+        fp.seek(offset)
+        buf = fp.read(size)
+
+        if buf == mal_str:
+                return True
+        else:
+                return False
+
