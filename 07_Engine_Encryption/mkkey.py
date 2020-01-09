91a4b66a5a3152c6ba679bb02b0b50e478368073
brutekoon <brutekoon@gmail.com>
2020-01-10 01:51:01 +0900

    rsa algorithm (not perfect)

---
+++ 07_Engine_Encryption/mkkey.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+
+import sys
+import k2rsa
+
+if __name__ == '__main__':
+    pu_fname = 'key.pkr'
+    pr_fname = 'key.skr'
+
+    if len(sys.argv) == 3:
+        pu_fname = sys.argv[1]
+        pr_fname = sys.argv[2]
+
+    k2rsa.create_key(pu_fname, pr_fname, True)
