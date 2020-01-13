eebd1836bcc5e9583bc57b8647a57c89340b4fa6
brutekoon <brutekoon@gmail.com>
2020-01-14 01:27:07 +0900

    modify source code and ReadMe

--- 07_Engine_Encryption/mkkey.py
+++ 07_Engine_Encryption/mkkey.py
@@ -10,5 +10,9 @@ if __name__ == '__main__':
     if len(sys.argv) == 3:
         pu_fname = sys.argv[1]
         pr_fname = sys.argv[2]
+    elif len(sys.argv) != 1:
+        print 'Usage : mkkey.py [pu] [pk] '
+        exit(0)
+
 
     k2rsa.create_key(pu_fname, pr_fname, True)
